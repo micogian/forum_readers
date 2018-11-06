33a65f1fb14a36e62d924b71abd5ed556f7b432b
micogian <giannidose@tin.it>
2018-11-06 14:57:59 +0100

    Create common.php

---
+++ language/en/common.php
@@ -0,0 +1,20 @@
+<?php
+/** 
+* 
+* @package Micogian - Forum Readers
+* @copyright (c) 2018 Micogian
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2 
+* 
+*/ 
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+$lang = array_merge($lang, array(
+	'READERS_1'			=> 'reader',
+	'READERS_2'			=> 'readers',
+));
